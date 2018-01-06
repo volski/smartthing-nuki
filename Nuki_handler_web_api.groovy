ce8ce215188178f111055d7c2a6a1f140569e7c2
volski <volski.alex@gmail.com>
2018-01-06 16:07:38 +0200

    fix duplicate code

--- Nuki_handler_web_api.groovy
+++ Nuki_handler_web_api.groovy
@@ -6,10 +6,9 @@ metadata {
         capability "Actuator"
         capability "Lock"      
         capability "Refresh" 
-        capability "Actuator"
         capability "Health Check"
-        
-    	
+        capability "Battery"
+
         command "refresh"
         command "lock"
         command "unlock"
@@ -22,8 +21,6 @@ metadata {
 	}
 
     tiles {
-    
-    
 		standardTile("door", "lock", width: 3, height: 2, canChangeIcon: true) {
         	state "locking", label: "Locking", action:"lock.unlock", icon:"st.locks.lock.locked", backgroundColor:"#F00000"
 			state "locked", label: "Locked", action:"lock.unlock", icon:"st.locks.lock.locked", backgroundColor:"#FF0000",nextState:"unlocking"
@@ -31,21 +28,26 @@ metadata {
             state "unlocked", label: "Unlocked", action:"lock.lock", icon:"st.locks.lock.unlocked", backgroundColor:"#79b821",nextState:"locking"
             state "unlocking", label: "Unlocking", action:"lock.lock", icon:"st.locks.lock.unlocked", backgroundColor:"#79b711"
             state "lockngo",label: "LockNgo - UNLOCKED",action: "wait", icon:"st.locks.lock.locked", backgroundColor:"#c0c123"
-		}
+            }
         
          standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 1, height: 1) {
         	state "default", action:"refresh", icon:"st.secondary.refresh"
     	}
+       
         
-        standardTile("lockNgo", "lock", inactiveLabel: false,  width: 2, height: 2) {
-        	state "locked",label:"Lock'N'Go", action:"lockNgo", icon:"st.locks.lock.locked", backgroundColor:"#FF0000"
-            state "unlocked",label:"Lock'N'Go", action:"lockNgo", icon:"st.locks.lock.locked", backgroundColor:"#79b821"
+        standardTile("lockNgo", "lock", width: 2, height: 2, canChangeIcon: true) {
+        	state "locked",label:"Lock'N'Go", action:"lockNgo", icon:"st.locks.lock", backgroundColor:"#FF0000"
+            state "unlocked",label:"Lock'N'Go", action:"lockNgo", icon:"st.locks.lock", backgroundColor:"#79b821"
     	}
+        valueTile("battery", "device.battery", decoration: "flat", width: 1, height: 1) {
+			state "battery", label:'Battery: ${currentValue}%', unit:"%"
+		}
+        
     }
 }
 
 def lock() {
-    log.trace "---LOCK COMMAND--- ${DevicePathOff}"
+    log.trace "---LOCK COMMAND---"
     def object = new JsonSlurper().parseText('{ "action": "2"}') 
     def options = [
         uri: "https://api.nuki.io/smartlock/${nukiId}/action",
@@ -99,18 +101,29 @@ def lockNgo(){
     }
     
 def refresh() {
+	sendEvent( name: "lock" ,value: "waiting")
 	log.trace "Refreshing!!"
     def params = [
     uri: "https://api.nuki.io/smartlock/${nukiId}",
     requestContentType: "application/json",
     headers: ['Authorization': "Bearer ${api_token}"]
   			]
+    try{
 	httpGet(params)
 		{resp ->           
         	log.debug "resp data: ${resp.data}"
+            log.debug "Battery is Critical: ${resp.data.state.batteryCritical}"
+            	if(resp.data.state.batteryCritical==false)
+           		{
+            	sendEvent(name:"battery", value:100)
+               	}
+                else
+                {
+                sendEvent(name:"battery", value:0)
+                }
         		if(1==resp.data.state.state)
                 {
-                 sendEvent(name: "lock", value: "locked")
+                sendEvent(name: "lock", value: "locked")
                 }
                 else if (2==resp.data.state.state)
                 {
@@ -133,10 +146,17 @@ def refresh() {
                 sendEvent(name: "lock", value: "waiting")
                 }
 			}
+        }
+        catch(e)
+        {
+        log.debug "Error ${e}"
+        }
+        
 	}
 
 def installed() {
     log.info('called shade installed()')
+    sendEvent( name: "lock" ,value: "unlocked")
     runEvery1Minute(refresh)
 }
 
@@ -144,10 +164,7 @@ def updated() {
     log.info('called shade updated()')
     runEvery1Minute(refresh)
 }
-def initialize() {
-	log.debug "Initialized"
-	runEvery1Minute(refresh)
-}
+
 
 
 
